9a114a505c0ae4f2f2e179b2759d9aefc027e713
Bogdan <bogdanmalyssh@gmail.com>
2026-04-09 01:58:06 +0200

    added money instead of score

--- game.py
+++ game.py
@@ -1,25 +1,24 @@
 import random
 
-score = 0
 
+money = 1
 while True:
+    print(f"You have {money} $")
     user = input("Higher or lower than 0? -> ")
     number = random.randint(1,10) * random.choice([-1,1])
     if number < 0 and user == "l":
         print("winner!", number)
-        score += 1
+        money += 1
     if number < 0 and user == "h":
         print("loser!", number)
-        if score > 0:
-            score -= 1
+        money -= 1
     if number > 0 and user == "l":
         print("loser!", number)
-        if score > 0:
-            score -= 1
+        money -= 1
     if number > 0 and user == "h":
         print("winner!", number)
-        score += 1
-    print(f"The score is: {score}")
+        money += 1
+    print(f"The money is: {money}")
     loop = input("Again? y/n -> ")
     if loop == "n":
         break
