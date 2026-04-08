0506cffc9e40415407f5fb48250d9e5cb8b15d30
Bogdan <bogdanmalyssh@gmail.com>
2026-04-09 01:21:17 +0200

    added while loop

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import random
 
-user = input("Higher or lower than 0? -> ")
-number = random.randint(1,10) * random.choice([-1,1])
-if number < 0 and user == "lower":
-    print("winner!", number)
-if number < 0 and user == "higher":
-    print("loser!", number)
-if number > 0 and user == "lower":
-    print("loser!", number)
-if number > 0 and user == "higher":
-    print("winner!", number)
+
+while True:
+    user = input("Higher or lower than 0? -> ")
+    number = random.randint(1,10) * random.choice([-1,1])
+    if number < 0 and user == "lower":
+        print("winner!", number)
+    if number < 0 and user == "higher":
+        print("loser!", number)
+    if number > 0 and user == "lower":
+        print("loser!", number)
+    if number > 0 and user == "higher":
+        print("winner!", number)
+    loop = input("Again? Yes/No -> ")
+    if loop == "no":
+        break
